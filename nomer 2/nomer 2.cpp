b48afa4d233c893ee9b6e8f66e5eff5be031589a
krusberk <katya.kotel@bk.ru>
2024-11-19 11:14:29 +0300

    sdfdsf

---
+++ nomer 2/nomer 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    double h;
+    double r;
+    const double p = 3.141592;
+    std::cout << "Введите высоту: ";
+    std::cin >> h;
+    std::cout << "Введите радиус: ";
+    std::cin >> r;
+    double v = p*pow(r,2)*h;
+    double s = p * pow(r, 2);
+    std::cout << "Объем кружки: " << v<<std::endl;
+    std::cout << "Площадь поверхности: " << s;
+}
+
