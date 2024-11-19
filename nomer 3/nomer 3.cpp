5d51a15b4631d263343d2aa527e857b1fef1dc07
krusberk <katya.kotel@bk.ru>
2024-11-19 11:49:36 +0300

    dsgdfg

---
+++ nomer 3/nomer 3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	double num;
+	double i;
+	std::cout << "Введите число: ";
+	std::cin >> num;
+	std::cout << "Округление в меньшую: " << floor(num)<< std::endl;
+	std::cout << "Округление в большую: " << ceil(num) << std::endl;
+	std::cout << "Округление от 0.5 в большую: " << round(num) << std::endl;
+	double n = modf(num,&i);
+	std::cout << "Дробная часть: " << n << std::endl;
+}
+
