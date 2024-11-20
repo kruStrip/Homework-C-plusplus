2fc4e332fd69b4568c75aae930f018ae95da9d1d
krusberk <katya.kotel@bk.ru>
2024-11-20 11:31:47 +0300

    sdffsdf

--- nomer 3/nomer 3.cpp
+++ nomer 3/nomer 3.cpp
@@ -1,17 +1,18 @@
 ﻿#include <iostream>
-#include <cmath>
 
 int main()
 {
 	setlocale(LC_ALL, "");
 	double num;
-	double i;
 	std::cout << "Введите число: ";
 	std::cin >> num;
-	std::cout << "Округление в меньшую: " << floor(num)<< std::endl;
-	std::cout << "Округление в большую: " << ceil(num) << std::endl;
-	std::cout << "Округление от 0.5 в большую: " << round(num) << std::endl;
-	double n = modf(num,&i);
-	std::cout << "Дробная часть: " << n << std::endl;
-}
-
+	std::cout << "Округление в меньшую: " << int(num)<< std::endl;
+	std::cout << "Округление в большую: " << (int(num)+1) << std::endl;
+	if ((num - int(num)) >= 0.5) {
+		std::cout << "Округление от 0.5 в большую: " << int(num)+1 << std::endl;
+	}
+	else {
+		std::cout << "Округление от 0.5 в большую: " << int(num) << std::endl;
+	}
+	std::cout << "Дробная часть: " << num - int(num) << std::endl;
+} 
