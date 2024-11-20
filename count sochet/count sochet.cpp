73dd80df482f7e190a67f7281f9f22a2d83720d0
krusberk <katya.kotel@bk.ru>
2024-11-20 12:56:33 +0300

    asda

---
+++ count sochet/count sochet.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void sochet(int k, int n) {
+    int res = 1;
+    for (int i = 1; i <= k; i++) {
+        res = res * (n - k + i) / i;
+    }
+    std::cout << "Кол-во сочетаний: "<< res;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int k;
+    int n;
+    std::cout << "Введите k: ";
+    std::cin >> k;
+    std::cout << "Введите n: ";
+    std::cin >> n;
+    sochet(k, n);
+}
