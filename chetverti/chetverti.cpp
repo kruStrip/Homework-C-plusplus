88fb9fc50c026b42885b8734311b732e691425ab
krusberk <katya.kotel@bk.ru>
2024-11-22 12:58:21 +0300

    укукук

---
+++ chetverti/chetverti.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int x;
+    int y;
+    std::cout << "Введите x: ";
+    std::cin >> x;
+    std::cout << "Введите y: ";
+    std::cin >> y;
+    if (x > 0) {
+        if (y > 0) {
+            std::cout << "Первая четверть";
+        }
+        else if (y < 0) {
+            std::cout << "Четвертая четверть";
+        }
+        else {
+            std::cout << "Первая и четвертая четверть";
+        }
+    }
+    else if (x < 0){
+        if (y > 0) {
+            std::cout << "Вторая четверть";
+        }
+        else if (y < 0) {
+            std::cout << "Третья четверть";
+        }
+        else {
+            std::cout << "Вторая и третья четверть";
+        }
+    }
+    else {
+        if (y > 0) {
+            std::cout << "Первая и вторая четверть";
+        }
+        else if (y < 0) {
+            std::cout << "Третья и четвертая четверть";
+        }
+    }
+}
