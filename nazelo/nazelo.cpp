fecdf86c0c284e5367b6dc803ad5285e8c4a6645
krusberk <katya.kotel@bk.ru>
2024-11-22 11:17:55 +0300

    reterbt

---
+++ nazelo/nazelo.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+bool check(int a, int m) {
+    if (a % m == 0) {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int a;
+    int m;
+    std::cout << "Введите число а: ";
+    std::cin >> a;
+    std::cout << "Введите число m: ";
+    std::cin >> m;
+    if (check(a, m)) {
+        std::cout << "Делится";
+    }
+    else {
+        std::cout << "Не делится";
+    }
+}
