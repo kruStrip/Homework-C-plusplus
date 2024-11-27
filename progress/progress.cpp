e84022eab0b283dad5659b8bc030816bb89fdf93
krusberk <katya.kotel@bk.ru>
2024-11-27 11:54:01 +0300

    gsdvghfd

---
+++ progress/progress.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int  sum_progress(int q, int b_0, int n) {
+    int sum = b_0;
+    for (int i = 1; i < n; i++) {
+        b_0 = b_0 * q;
+        sum += b_0;
+    }
+    return sum;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int n;
+    int q;
+    int b_0;
+    std::cout << "Введите q: ";
+    std::cin >> q;
+    std::cout << "Введите b0: ";
+    std::cin >> b_0;
+    std::cout << "Введите n: ";
+    std::cin >> n;
+
+    std::cout << sum_progress(q, b_0, n);
+}
+
