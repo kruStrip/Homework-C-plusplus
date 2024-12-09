c0d9d8504dc3f8c87088e74ae31363682535608e
krusberk <katya.kotel@bk.ru>
2024-12-09 09:43:41 +0300

    sdfdf

--- dva massiva/dva massiva.cpp
+++ dva massiva/dva massiva.cpp
@@ -20,6 +20,22 @@ int summ_poboch(int nums[4][4]) {
     return sum;
 }
 
+int razni(int nums_pryam[3][6]) {
+    int sum_chet = 0;
+    int sum_nechet = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int g = 0; g < 4; g++) {
+            if ((i + g) % 2 == 0) {
+                sum_chet += nums_pryam[i][g];
+            }
+            else {
+                sum_chet += nums_pryam[i][g];
+            }
+        }
+    }
+    return sum_chet - sum_nechet;
+}
+
 int main()
 {
     setlocale(LC_ALL, "");
@@ -35,5 +51,15 @@ int main()
     std::cout << "\nСумма элементов на главной диагонали: " << summ_glavnya(nums);
     std::cout << "\nСумма элементов на побочной диагонали: " << summ_poboch(nums);
 
+    int nums_pryam[3][6] = { {1,2,3,4,5,6}, {5,6,7,8,9,0}, {1,2,3,4, 5,6} };
+    std::cout << "\nПрямоугольный массив:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int g = 0; g < 6; g++) {
+            std::cout << nums_pryam[i][g] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "\nРазница суммы элементов в четной ячейке и суммы элементов в нечетной ячейке: " << razni(nums_pryam);
 }
 
