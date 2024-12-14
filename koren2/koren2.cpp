0306b6af3be12112897ccee1ef7fede3b82a7aa1
Екатерина котельникова <katya.kotel@bk.ru>
2024-12-15 01:27:50 +0300

    asda

---
+++ koren2/koren2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+double raschet(double num, double k) {
+    double x = num / k;
+    for (int i = 0;i < 100;i++) {
+        x = 0.5 * (x + num / pow(x, k-1));
+    }
+    return x;
+}
+
+int main()
+{
+    setlocale(0, "");
+    double num;
+    int k;
+    std::cout << "Введите число: ";
+    std::cin >> num;
+    std::cout << "Введите степень: ";
+    std::cin >> k;
+    if (k < 2) {
+        std::cout << "Основание должно быть >=2";
+    }
+    std::cout << "Квадратный корень, числа " << num <<" и степени "<< k << " равен " << raschet(num, k);
+}
