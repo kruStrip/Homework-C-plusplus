fc1105c84afabbd02238f5403b53b0a74103abd7
Екатерина котельникова <katya.kotel@bk.ru>
2024-12-15 01:18:36 +0300

    asdad

---
+++ koren/koren.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+double raschet(double num) {
+    double x = num / 2;
+    for (int i = 0;i < 100;i++) {
+        x = 0.5 * (x + num / x);
+    }
+    return x;
+}
+
+int main()
+{
+    setlocale(0, "");
+    double num;
+    std::cout << "Введите число: ";
+    std::cin >> num;
+    std::cout << "Квадратный корень, числа " << num << " равен " << raschet(num);
+}
