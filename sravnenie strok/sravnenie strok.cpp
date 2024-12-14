6cee5eebaa936853db17065f137ffc1609e37a57
Екатерина котельникова <katya.kotel@bk.ru>
2024-12-15 01:38:42 +0300

    asdasd

---
+++ sravnenie strok/sravnenie strok.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+bool check(std::string str1, std::string str2) {
+
+    if (str1.size() != str2.size()) {
+        return false;
+    }
+
+    for (int i = 0; i < str1.size(); i++) {
+        if (str1[i] != str2[i]) {
+            return false;
+        }
+    }
+}
+
+int main()
+{
+    setlocale(0, "");
+    std::string str1;
+    std::string str2;
+    std::cout << "Введите превое слово: ";
+    std::cin >> str1;
+    std::cout << "Введите второе слово: ";
+    std::cin >> str2;
+
+    if (check(str1, str2)) {
+        std::cout << "Строки совпадают!";
+    }
+    else {
+        std::cout << "Строки не совпадают!";
+    }
+}
