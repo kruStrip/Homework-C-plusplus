386f52a647b69fd38399ec881eb0c04f14ed2c01
kruStrip <katya.kotel@bk.ru>
2025-01-22 09:42:18 +0300

    asdsd

---
+++ Dz po ykazatelyam №2/Dz po ykazatelyam №2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+void opredelenie(char* preob) {
+    if (preob[0] == 0x00) {
+        std::cout << "Порядок следования байт: big-endian";
+    }
+    else if (preob[1] == 0x00) {
+        std::cout << "Порядок следования байт: little-endian";
+    }
+}
+
+int main()
+{
+    setlocale(0, "");
+    short int num = 0xFF00;
+    char* preob = (char*)&num;
+    opredelenie(preob);
+}
